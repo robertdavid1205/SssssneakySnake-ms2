892ac2eddea913443b7ace8c3dd8878ce1e4ad75
robertdavid1205 <robertdavid1205@gmail.com>
2021-08-23 17:40:23 +0000

    Added endGame function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -24,3 +24,18 @@ window.onload = function() {
     document.getElementById("game-score").innerHTML = "";
     return setInterval(draw, 1000 / x);
 }
+
+function pauseGame() {
+    // setting gameOn flag to false
+    clearInterval(gameControl);
+    gameOn = false;
+    document.getElementById("game-status").innerHTML = "<small>Game Paused</small>";
+}
+
+function endGame(x) {
+    // setting gameOn flag to false
+    clearInterval(gameControl);
+    gameOn = false;
+    document.getElementById("game-status").innerHTML = "<small>Game Over</small>";
+    document.getElementById("game-score").innerHTML = "<h1>Score: " + x + "</h1>";
+}
